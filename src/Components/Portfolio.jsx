56ef71da91192291805b8a271d4e7c321a7a3356
HINA ASAD <124295338+hina672@users.noreply.github.com>
2024-05-20 18:19:07 +0500

    add changes again

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,16 @@ const imageAltText = "data image";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Basics Of Python For DataScience 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Collaboration to create a beginner friendly Repository on Python.",
+    url: "https://hina672.github.io/Basics-Of-Python-For-Data-Science/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "SQL for Data Science",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Collaboration to create a beginner friendly Repository on SQL.",
+    url: "https://hina672.github.io/SQL-For-Data-Science-With-Python/",
   },
 ];
 
